053d4d344cbea2b0e8b87a821c507980dfdce0db
FatLorenzo <mail@larshermes.de>
2014-12-20 13:28:27 +0100

    Create simple test function for function makeCacheMatrix

---
+++ testing.R
@@ -0,0 +1,30 @@
+## Programming Assignment: Caching the Inverse of a Matrix
+## Author:  Lars Hermes
+## Date:    Dec. 20th, 2014
+## Version: 0.1
+
+## Some sample matrices for testing purposes
+a=rbind(c(2, 1), c(1, 1))
+b=rbind(c(1, -1), c(-1, 2))
+
+## Simple test case if makeCacheMatrix works.
+testMakeCacheMatrix <- function() {
+    print("Matrix a:")
+    print(a)
+    print("Calling makeCacheMatrix with matrix a: m <- makeCacheMatrix(a)")
+    m <- makeCacheMatrix(a)
+    print("Get internal stored matrix of m and assigning it to c: c <- m$get()")
+    c <- m$get()
+    print("Matrix c:")
+    print(c)
+    print("Check wether if a and c are equal")
+    print(identical(a,c))
+    print("Check wether if a and internal stored value are identical")
+    print(identical(a,m$get()))
+    print("Matrix b:")
+    print(b)
+    print("Store matrix b in m: m$set(b)")
+    m$set(b)
+    print("Check wether if b and internal stored value are identical")
+    print(identical(b,m$get()))
+} 
